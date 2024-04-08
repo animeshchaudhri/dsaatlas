c0ed9d60c8f54bb1a38bd267da67d946dab5e6ff
animesh <animeshchaudhri3@gmail.com>
2024-04-08 13:25:21 +0530

    idk what to do with ahead

--- components/Features/Feature_About/Code_Vault.tsx
+++ components/Features/Feature_About/Code_Vault.tsx
@@ -1,131 +1,171 @@
+// "use client";
+// import React, { useRef } from "react";
+// import {
+//   useScroll,
+//   useTransform,
+//   motion,
+//   MotionValue,
+//   easeIn,
+// } from "framer-motion";
+// import SmoothScroll from "./Smooth.jsx";
+// import { Sidebar } from "@/components/ui/Navbar/Sidebar";
+// export type Playlist = (typeof playlists)[number];
+// export const playlists = [
+//   "Arrays 101",
+//   "Linked Lists",
+//   "Stacks ",
+//   "Queues",
+//   "Graph ",
+//   "Sorting",
+//   "Searching ",
+//   "Greedy",
+//   "Hashing",
+//   "Heap",
+// ];
+
+// export const ContainerScroll = ({
+//   titleComponent,
+//   children,
+// }: {
+//   titleComponent: string | React.ReactNode;
+//   children: React.ReactNode;
+// }) => {
+//   const containerRef = useRef<any>(null);
+//   const { scrollYProgress } = useScroll({
+//     target: containerRef,
+//   });
+//   const [isMobile, setIsMobile] = React.useState(false);
+
+//   React.useEffect(() => {
+//     const checkMobile = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+//     checkMobile();
+//     window.addEventListener("resize", checkMobile);
+//     return () => {
+//       window.removeEventListener("resize", checkMobile);
+//     };
+//   }, []);
+
+//   const scaleDimensions = () => {
+//     return isMobile ? [0.7, 0.9] : [1.05, 1];
+//   };
+
+//   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+//   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
+//   const translate = useTransform(scrollYProgress, [0, 1], [0, -60]);
+//   const translate2 = useTransform(scrollYProgress, [0, 1], [0, -100]);
+//   return (
+//     <div
+//       className=" flex items-center justify-center relative  "
+//       ref={containerRef}
+//     >
+//       <div
+//         className=" w-full relative"
+//         style={{
+//           perspective: "1000px",
+//         }}
+//       >
+//         <Header translate={translate} titleComponent={titleComponent} />
+//         <Card rotate={rotate} translate={translate2} scale={scale}>
+//           {children}
+//         </Card>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export const Header = ({ translate, titleComponent }: any) => {
+//   return (
+//     <motion.div
+//       style={{
+//         translateY: translate,
+//       }}
+//       className="div max-w-5xl mx-auto text-center"
+//     >
+//       {titleComponent}
+//     </motion.div>
+//   );
+// };
+
+// export const Card = ({
+//   rotate,
+//   translate,
+//   scale,
+//   children,
+// }: {
+//   rotate: MotionValue<number>;
+//   scale: MotionValue<number>;
+//   translate: MotionValue<number>;
+//   children: React.ReactNode;
+// }) => {
+//   return (
+//     <>
+//       <motion.div
+//         style={{
+//           rotateX: rotate,
+
+//           scale,
+//           boxShadow:
+//             "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+//         }}
+//         className="max-w-5xl   mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+//       >
+//         <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
+//           <div className="grid lg:grid-cols-5 ">
+//             <Sidebar playlists={playlists} className="hidden lg:block " />
+
+//             <motion.div
+//               style={{ translateY: translate }}
+//               className="col-span-3 lg:col-span-4 h-full"
+//             >
+//               {children}
+//             </motion.div>
+//           </div>
+//         </div>
+//       </motion.div>
+//     </>
+//   );
+// };
+
 "use client";
-import React, { useRef } from "react";
-import {
-  useScroll,
-  useTransform,
-  motion,
-  MotionValue,
-  easeIn,
-} from "framer-motion";
-import SmoothScroll from "./Smooth.jsx";
-import { Sidebar } from "@/components/ui/Navbar/Sidebar";
-export type Playlist = (typeof playlists)[number];
-export const playlists = [
-  "Arrays 101",
-  "Linked Lists",
-  "Stacks ",
-  "Queues",
-  "Graph ",
-  "Sorting",
-  "Searching ",
-  "Greedy",
-  "Hashing",
-  "Heap",
-];
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import Image from "next/image";
+import Placehold from "@/public/lc1.png";
+function Code_Vault() {
+  const cardX = useMotionValue(0);
+  const cardY = useMotionValue(0);
 
-export const ContainerScroll = ({
-  titleComponent,
-  children,
-}: {
-  titleComponent: string | React.ReactNode;
-  children: React.ReactNode;
-}) => {
-  const containerRef = useRef<any>(null);
-  const { scrollYProgress } = useScroll({
-    target: containerRef,
-  });
-  const [isMobile, setIsMobile] = React.useState(false);
+  const handleMouseMove = (event: any) => {
+    const offsetX = event.clientX - window.innerWidth / 2;
+    const offsetY = event.clientY - window.innerHeight / 2;
 
-  React.useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-    checkMobile();
-    window.addEventListener("resize", checkMobile);
-    return () => {
-      window.removeEventListener("resize", checkMobile);
-    };
-  }, []);
-
-  const scaleDimensions = () => {
-    return isMobile ? [0.7, 0.9] : [1.05, 1];
+    cardX.set(offsetX);
+    cardY.set(offsetY);
   };
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
-  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const translate = useTransform(scrollYProgress, [0, 1], [0, -60]);
-  const translate2 = useTransform(scrollYProgress, [0, 1], [0, -100]);
-  return (
-    <div
-      className=" flex items-center justify-center relative  "
-      ref={containerRef}
-    >
-      <div
-        className=" w-full relative"
-        style={{
-          perspective: "1000px",
-        }}
-      >
-        <Header translate={translate} titleComponent={titleComponent} />
-        <Card rotate={rotate} translate={translate2} scale={scale}>
-          {children}
-        </Card>
-      </div>
-    </div>
-  );
-};
+  const handleMouseLeave = () => {
+    cardX.set(0);
+    cardY.set(0);
+  };
 
-export const Header = ({ translate, titleComponent }: any) => {
   return (
     <motion.div
+      key="card"
       style={{
-        translateY: translate,
+        perspective: 800,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      className="div max-w-5xl mx-auto text-center"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
     >
-      {titleComponent}
+      <div className=" showcase overflow-hidden container  image-container  transform rounded-xl bg-gradient-to-r from-cyan-700 to-cyan-500 p-2 ">
+        <Image src={Placehold} priority={true} alt="somthing" />
+      </div>
     </motion.div>
   );
-};
+}
 
-export const Card = ({
-  rotate,
-  translate,
-  scale,
-  children,
-}: {
-  rotate: MotionValue<number>;
-  scale: MotionValue<number>;
-  translate: MotionValue<number>;
-  children: React.ReactNode;
-}) => {
-  return (
-    <>
-      <motion.div
-        style={{
-          rotateX: rotate,
-
-          scale,
-          boxShadow:
-            "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
-        }}
-        className="max-w-5xl   mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
-      >
-        <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
-          <div className="grid lg:grid-cols-5 ">
-            <Sidebar playlists={playlists} className="hidden lg:block " />
-
-            <motion.div
-              style={{ translateY: translate }}
-              className="col-span-3 lg:col-span-4 h-full"
-            >
-              {children}
-            </motion.div>
-          </div>
-        </div>
-      </motion.div>
-    </>
-  );
-};
-
-
+export default Code_Vault;
--- components/Features/Feature_About/Feature_About.tsx
+++ components/Features/Feature_About/Feature_About.tsx
@@ -1,32 +1,52 @@
-import { ContainerScroll } from "./Code_Vault";
-import Dummy from "../../Sheet/Dummy";
-import Titable from "../../ui/Cards/Titable";
+// import { ContainerScroll } from "./Code_Vault";
+// import Dummy from "../../Sheet/Dummy";
+// import Titable from "../../ui/Cards/Titable";
+// function Feature_About() {
+//   return (
+//     <>
+//       <div className="flex h-[60rem] md:h-[80rem] flex-col justify-center items-center gap-10 max-md:mt-10">
+//         <ContainerScroll
+//           titleComponent={
+//             <>
+//               <div className="flex flex-col items-center overflow-hidden justify-center">
+//                 <h1 className="mb-6  text-2xl font-semibold lg:text-4xl">
+//                   Real world questions for Technical Interviews
+//                 </h1>
+//                 <p className="mx-auto mb-9 max-w-2xl px-2 text-center text-sm leading-7 text-gray-400 lg:text-base">
+//                   There are a handful of machine coding questions that can be
+//                   asked in a Technical Interview. Dsa atlas provides a
+//                   comprehensive list of front-end and algorithmic questions with
+//                   solutions so that you are prepared, always.
+//                 </p>
+//               </div>
+//             </>
+//           }
+//         >
+//           <Dummy />
+//         </ContainerScroll>
+//         <Titable />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Feature_About;
+import Code_Vault from "./Code_Vault";
 function Feature_About() {
   return (
-    <>
-      <div className="flex h-[60rem] md:h-[80rem] flex-col justify-center items-center gap-10 max-md:mt-10">
-        <ContainerScroll
-          titleComponent={
-            <>
-              <div className="flex flex-col items-center overflow-hidden justify-center">
-                <h1 className="mb-6  text-2xl font-semibold lg:text-4xl">
-                  Real world questions for Technical Interviews
-                </h1>
-                <p className="mx-auto mb-9 max-w-2xl px-2 text-center text-sm leading-7 text-gray-400 lg:text-base">
-                  There are a handful of machine coding questions that can be
-                  asked in a Technical Interview. Dsa atlas provides a
-                  comprehensive list of front-end and algorithmic questions with
-                  solutions so that you are prepared, always.
-                </p>
-              </div>
-            </>
-          }
-        >
-          <Dummy />
-        </ContainerScroll>
-        <Titable />
-      </div>
-    </>
+    <div className="flex flex-col items-center mt-40  overflow-hidden ">
+      <h1 className="mb-6 text-left text-2xl font-semibold lg:text-4xl">
+        Real world questions for Technical Interviews
+      </h1>
+      <p className="mx-auto mb-6 max-w-2xl px-2 text-center text-sm leading-7 text-gray-400 lg:text-base">
+        There are a handful of machine coding questions that can be asked in a
+        Technical Interview. Dsa atlas provides a comprehensive list of
+        front-end and algorithmic questions with solutions so that you are
+        prepared, always.
+      </p>
+
+      <Code_Vault />
+    </div>
   );
 }
 
