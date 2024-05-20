f271475a966a175553715664ff22c57a73d07130
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-05-20 23:28:40 +0530

    Add new Api URL

--- app/api/sheet.tsx
+++ app/api/sheet.tsx
@@ -1,13 +1,13 @@
 export default async function sheetcall(Difficulty: string) {
   const data = fetch(
-    `https://helixsnagglefoot-collar.toystack.dev/${Difficulty}`
+    `https://lavenderlord-stinger.toystack.dev/${Difficulty}`
   );
   const response = await data;
 
   return response;
 }
 export async function companycall(company: string) {
-  const data = fetch(`https://helixsnagglefoot-collar.toystack.dev/specific?companies=${company}`);
+  const data = fetch(`https://lavenderlord-stinger.toystack.dev/specific?companies=${company}`);
   const response = await data;
 
   return response;
